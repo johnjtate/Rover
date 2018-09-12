7852d0ed5eaaed62589539f1a49499115bfd8eb9
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-12 16:15:05 -0600

    Restarted to use ObjectiveC

---
+++ Rover/Rover/Model/Rover.h
@@ -0,0 +1,13 @@
+//
+//  Rover.h
+//  Rover
+//
+//  Created by John Tate on 9/12/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rover : NSObject
+
+@end
---
+++ Rover/Rover/Model/Rover.m
@@ -0,0 +1,13 @@
+//
+//  Rover.m
+//  Rover
+//
+//  Created by John Tate on 9/12/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import "Rover.h"
+
+@implementation Rover
+
+@end
